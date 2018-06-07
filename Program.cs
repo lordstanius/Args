db03720b3bd5f5cac4dcb8f71d168fb70ec12109
Mladen Stanišić <mladen.stanisic@comdata.rs>
2018-06-07 08:44:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CmdLineParser
+{
+    class Program
+    {
+        static void Main()
+        {
+            string commandLine1 = "-b -i 42 -s Some_string";
+            string[] commandLine2 = { "/i", "42", "/s", "Some string" };
+            var parser = new Args("b,i#,s*", commandLine1.Split(' '));
+
+            Console.WriteLine("Boolean (b) value: {0}", parser.GetBool('b'));
+            Console.WriteLine("Integer (i) value: {0}", parser.GetInt('i'));
+            Console.WriteLine("String (s) value:  {0}", parser.GetString('s'));
+
+            Console.WriteLine("---------------------");
+            parser = new Args("b,i#,s*", commandLine2);
+
+            Console.WriteLine("Boolean (b) value: {0}", parser.Has('b') ? parser.GetBool('b').ToString() : "no value");
+            Console.WriteLine("Integer (i) value: {0}", parser.GetInt('i'));
+            Console.WriteLine("String (s) value:  {0}", parser.GetString('s'));
+        }
+    }
+}
---
+++ Test/ArgsTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using CmdLineParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ArgsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Args args = new Args("a,b,c,d", new[] { "-a", "-b", "/cd" });
+
+            Assert.IsTrue(args.Has('a') && args.Has('b') && args.Has('c') && args.Has('d'));
+            Assert.IsTrue(args.GetBool('a'));
+            Assert.IsTrue(args.GetBool('b'));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Args args = new Args("l,p#,d*", new[] { "-l", "/p", "80", "/d", "Test" });
+
+            Assert.IsTrue(args.Has('l') && args.Has('p') && args.Has('d'));
+            Assert.IsTrue(args.GetBool('l'));
+            Assert.IsTrue(args.GetInt('p') == 80);
+            Assert.AreEqual(args.GetString('d'), "Test");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string[] test = { "Test", "Another test", "Yet another test" };
+            Args args = new Args("p##,d[*]", new[] { "/p", "8.04", "/d" }.Concat(test));
+
+            Assert.IsTrue(args.Has('p') && args.Has('d'));
+            Assert.IsTrue(args.GetDouble('p') == 8.04);
+
+            string[] result = args.GetStringArray('d');
+            for (int i = 0; i < test.Length; ++i)
+                Assert.AreEqual(test[i], result[i]);
+        }
+    }
+}
